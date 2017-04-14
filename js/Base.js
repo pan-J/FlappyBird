976fb34965c593696e5507a1aa6a30df7db6b7da
pp <pipi@pp.com>
2017-04-14 22:25:50 +0800

    FlappyBird

---
+++ js/Base.js
@@ -0,0 +1,15 @@
+(function() {
+
+var Base = function(options) {
+	this.ctx = options.ctx;
+
+	this.speed = 2;
+	this.x = 0;
+	this.y = 0;
+};
+
+Fly.getBase = function(options) {
+	return new Base(options);
+};
+
+})(Fly);
